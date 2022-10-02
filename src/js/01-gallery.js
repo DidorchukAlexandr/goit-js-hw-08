f1661178be39cbdf9e5694846edc2171f8bf6b0f
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2022-10-03 00:08:14 +0300

    homework 8 js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>`;
+    }).join("");
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+const throttle = require("lodash.throttle");
 
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+const submitButoon = document.querySelector("button");
+const data = { email: "", message: "" };
+
+email.addEventListener("input", addData);
+message.addEventListener("input", addData);
+submitButoon.addEventListener("click", userData);
+
+checkData();
+
+function checkData() {
+  const localData = JSON.parse(localStorage.getItem("feedback-form-state"));
+  if (localData) {
+    email.value = localData.email;
+    message.value = localData.message;
+    data.email = localData.email;
+    data.message = localData.message;
+  }
+}
+
+const saveData = throttle(function () {
+  localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}, 500);
+
+function addData(e) {
+  data[this.name] = e.target.value;
+  saveData();
+}
+
+function userData(e) {
+  e.preventDefault();
+  const localData = JSON.parse(localStorage.getItem("feedback-form-state"));
+  if (!email.value || !message.value) {
+    console.log("error: not all fields are filled");
+    alert("Please fill all fields");
+  } else {
+    console.log(localData);
+    localStorage.removeItem("feedback-form-state");
+    data.email = "";
+    data.message = "";
+    form.reset();
+  }
+}
